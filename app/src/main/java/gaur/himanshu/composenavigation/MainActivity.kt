cb29fc104bce1b9d75a4810802e0ef7cb083f207
himanshugaur <himanshu@spare8.com>
2024-07-21 11:09:21 +0530

    Nested Navigation

--- app/src/main/java/gaur/himanshu/composenavigation/MainActivity.kt
+++ app/src/main/java/gaur/himanshu/composenavigation/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
             ComposeNavigationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)) {
-                        MainNavigation()
+
                     }
                 }
             }
